ee042ddae67a25f61e53fa6abad824d93bbb15fc
aurelien pochart <aurelien.pochart@epitech.eu>
2024-05-18 16:07:24 +0200

    feat(game_page): drag and drop

--- scenes/game/views/elements.lua
+++ scenes/game/views/elements.lua
@@ -37,12 +37,29 @@ local function onDraw()
 end
 
 local function onLoaded()
-    cursor = love.mouse.setVisible(true)
+    love.mouse.setVisible(true)
 end
 
 local function onUpdate(button)
     if love.mouse.isDown(1) then
-        print("Mouse")
+        local x, y = love.mouse.getPosition()
+        if x > sodium.positionX and x < sodium.positionX + 71 and y > sodium.positionY and y < sodium.positionY + 71 then
+            sodium:setScalePosition(0.3, 0.305, x - 35, y - 35)
+        elseif x > magnesium.positionX and x < magnesium.positionX + 71 and y > magnesium.positionY and y < magnesium.positionY + 71 then
+            magnesium:setScalePosition(0.345, 0.31, x - 35, y - 35)
+        elseif x > soufre.positionX and x < soufre.positionX + 71 and y > soufre.positionY and y < soufre.positionY + 71 then
+            soufre:setScalePosition(0.34, 0.345, x - 35, y - 35)
+        elseif x > potassium.positionX and x < potassium.positionX + 71 and y > potassium.positionY and y < potassium.positionY + 71 then
+            potassium:setScalePosition(0.427, 0.438, x - 35, y - 35)
+        elseif x > cuivre.positionX and x < cuivre.positionX + 71 and y > cuivre.positionY and y < cuivre.positionY + 71 then
+            cuivre:setScalePosition(0.44, 0.45, x - 35, y - 35)
+        end
+    else 
+        sodium:setScalePosition(sodInit[1], sodInit[2], sodInit[3], sodInit[4])
+        magnesium:setScalePosition(magInit[1], magInit[2], magInit[3], magInit[4])
+        soufre:setScalePosition(souInit[1], souInit[2], souInit[3], souInit[4])
+        potassium:setScalePosition(potInit[1], potInit[2], potInit[3], potInit[4])
+        cuivre:setScalePosition(cuiInit[1], cuiInit[2], cuiInit[3], cuiInit[4])
     end
 end
 
